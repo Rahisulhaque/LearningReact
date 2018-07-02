e280ef26a6a43c091534e20635b3fa486a3d3ae8
Rahisul Haque <rahisul@icloud.com>
2018-07-01 23:07:26 -0700

    Learning React

---
+++ React/functionRecap.js
@@ -0,0 +1,21 @@
+// ***************************************************************************************** //
+//                                                                                           //
+//                                                                                           //
+//   function.js                                                     __    _            __   //
+//                                                       _________ _/ /_  (_)______  __/ /   //
+//   By: rahisul <rahisul@icloud.com>                   / ___/ __ `/ __ \/ / ___/ / / / /    //
+//                                                     / /  / /_/ / / / / (__  ) /_/ / /     //
+//   Created: 2018/07/01 14:43:14 by rahisul          /_/   \__,_/_/ /_/_/____/___,_/_/      //
+//   Updated: 2018/07/01 14:50:38 by rahisul                                                 //
+//                                                                                           //
+// ***************************************************************************************** //
+
+/*This is a quick recap of functions */
+
+function getDistance(speed , time){
+		return speed * time + " miles."
+}
+
+
+console.log("the result is :" + getDistance(60, 15));
+console.log("the result is :" + getDistance(90, 4));
